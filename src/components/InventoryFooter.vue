4a83034b9a968a8e8bea0956be6144bc009ef37d
AbibullaevAR <a.alim2019@mail.ru>
2025-03-24 09:52:59 +0300

    add close button to footer

--- src/components/InventoryFooter.vue
+++ src/components/InventoryFooter.vue
@@ -1,10 +1,12 @@
 <script lang="ts" setup>
 import Skeleton from '@/components/ui/Skeleton.vue';
+import CloseButton from '@/components/ui/CloseButton.vue';
 </script>
 
 <template>
   <div class="inventory-footer">
-    <Skeleton :width="'80%'" :height="'36px'" />
+    <CloseButton class="inventory-footer__close-button" />
+    <Skeleton :width="'95%'" :height="'36px'" />
   </div>
 </template>
 
@@ -13,5 +15,12 @@ import Skeleton from '@/components/ui/Skeleton.vue';
   padding: 18px;
   border: 1px solid var(--color-grey);
   border-radius: 12px;
+  position: relative;
+
+  &__close-button {
+    position: absolute;
+    top: 8px;
+    right: 8px;
+  }
 }
 </style>
